22478f676276cb0c4c607b6a302083838ea4939e
esref <esref.viduslu@gmail.com>
2016-08-12 10:50:52 +0300

    new create

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,9 @@
 class PlacesController < ApplicationController
+	
+	def new
+		@place = Place.new
+	end
+
 	def index
 		@places = Place.all.order(:id)
 	end
@@ -7,5 +12,18 @@ class PlacesController < ApplicationController
 		@places = Place.find(params[:id])
 	end
 
+	def create 
+		@place = Place.new(place_params)
+		
+		if @place.save
+			redirect_to place_path(@place)
+		else
+			render :new
+		end
+	end
+
+	def place_params
+		params.permit(:name, :address, :phone_number, :contact_mail)
+	end
 		
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 	root  'places#index'
+	get 'places/new', to: 'places#new'
 	get 'places', to: 'places#index'
-	get 'places/:id', to: 'places#show'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+	get 'places/:id', to: 'places#show', as:"place"
+	post 'places', to: 'places#create'
 end
